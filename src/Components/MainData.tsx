57f69c9294f8fc2e2ec2345adaac8c1e2d5af83d
rohan <rohanthakur89768@gmail.com>
2024-04-02 10:40:35 +0530

    added layout of sidebar and maindata

--- src/Components/MainData.tsx
+++ src/Components/MainData.tsx
@@ -1,7 +1,9 @@
 export default function MainData(){
     return(
-        <>
+        <div className="basis-9/12 h-full
+         text-black rounded-tr-3xl rounded-br-3xl bg-[#f6f6f8]"
+         >
          maindata
-        </>
+        </div>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import MainData from "../Components/MainData";
 import Sidebar from "../Components/Sidebar";
-
+import nightimg from '../Assets/nightimg.jpg';
 export default function Home(){
     return(
-        <>
-        <div>
+        <div className="h-[100vh]"
+         style={{backgroundImage : `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${nightimg})`}}>
+           <div className="h-[90%]  mx-8 py-8 flex flex-row justify-center items-center" >
             <Sidebar/>
             <MainData/>
         </div>
       
-        </>
+        </div>
     )
 }

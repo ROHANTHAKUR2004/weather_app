4b4c1a6e431f8f3206a27f536a8ed4e6d19ea75e
rohan <rohanthakur89768@gmail.com>
2024-04-03 16:30:22 +0530

    added fetch data asynk thunk

--- src/Redux/Slice/ForecastSlice.ts
+++ src/Redux/Slice/ForecastSlice.ts
@@ -1,5 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import ForecastDataState from "../../Interfaces/ForecastDataState";
+import axiosinstance from "../../Config/axiosInstance";
+
 
 const initialState : ForecastDataState = {
     status : 'default',
@@ -7,12 +9,26 @@ const initialState : ForecastDataState = {
 }
 
 
+export const fetchData = createAsyncThunk('data/fetcghdata',  async() => {
+    try {
+        const response =  await axiosinstance.get(`forecast.json?key=995967ca3cd04f8f92a32058240204&days=7&aqi=yes&q=Bhopal`)
+       console.log(response);
+       return response;
+    }
+     catch (error) {
+       console.log(error) 
+    }
+});
+
 
 const forecastSlice = createSlice({
     name : 'forecast',
     initialState ,
     reducers : {},
-    extraReducers: () => {
+    extraReducers: (builder) => {
+        builder.addCase(fetchData.fulfilled, ( state , action )=>{
+            if(!action.payload) return ;
+        })
 
     }
 });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import MainData from "../Components/Maindata/MainData";
 import Sidebar from "../Components/sidebar/Sidebar";
 import nightimg from '../Assets/nightimg.jpg';
+import { useEffect } from "react";
+import { fetchData } from "../Redux/Slice/ForecastSlice";
+import { useAppDispatch } from "../hooks/hooks";
 export default function Home(){
+
+    const dispatch = useAppDispatch();
+
+    useEffect(()=>{
+       dispatch(fetchData());
+    },[]);
+
     return(
         <div className="min-h-[100vh]  px-8 py-8 flex flex-row justify-center items-stretch"
          style={{backgroundImage : `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(${nightimg})`}}>
